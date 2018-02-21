3aefbd6025322072b756bf4f1da5c7453ea77bef
chenmtech <chenm91@163.com>
2018-02-21 10:29:52 +0800

    整理Complex类
    设为final类；
    修改了加减一个实数的运算规则；

--- src/com/cmtech/dsp/seq/Complex.java
+++ src/com/cmtech/dsp/seq/Complex.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 /**
  * ClassName: Complex
- * Function: 复数. 
+ * Function: 复数类. 
  * Reason: TODO ADD REASON(可选). 
  * date: 2018年2月2日 上午6:00:36 
  *
@@ -12,18 +12,16 @@ import java.io.Serializable;
  * @version 
  * @since JDK 1.6
  */
-public class Complex implements Serializable{
-	/**
-	 * serialVersionUID:TODO(用一句话描述这个变量表示什么).
-	 * @since JDK 1.6
-	 */
+public final class Complex implements Serializable{
+
 	private static final long serialVersionUID = 1L;
 	private double real;
 	private double imag;
 	
 	/**
+	 * 
 	 * Creates a new instance of Complex.
-	 *
+	 * 实部和虚部都为0.0
 	 */
 	public Complex()
 	{
@@ -31,12 +29,25 @@ public class Complex implements Serializable{
 		imag = 0.0;
 	}
 	
+	/**
+	 * 
+	 * Creates a new instance of Complex.
+	 *
+	 * @param re 实部
+	 * @param im 虚部
+	 */
 	public Complex(double re, double im)
 	{
 		real = re;
 		imag = im;
 	}
 	
+	/**
+	 * 
+	 * Creates a new instance of Complex.
+	 *
+	 * @param c 复数
+	 */
 	public Complex(Complex c) {
 		real = c.real;
 		imag = c.imag;
@@ -46,95 +57,251 @@ public class Complex implements Serializable{
 	 * getReal:获取实部
 	 *
 	 * @author bme
-	 * @return
+	 * @return 实部
 	 * @since JDK 1.6
 	 */
 	public double getReal() {
 		return real;
 	}
 
+	/**
+	 * 
+	 * setReal: 设置实部
+	 * TODO(这里描述这个方法适用条件 – 可选)
+	 * TODO(这里描述这个方法的执行流程 – 可选)
+	 * TODO(这里描述这个方法的使用方法 – 可选)
+	 * TODO(这里描述这个方法的注意事项 – 可选)
+	 *
+	 * @author bme
+	 * @param real 实部
+	 * @since JDK 1.6
+	 */
 	public void setReal(double real) {
 		this.real = real;
 	}
 
+	/**
+	 * 
+	 * getImag: 获取虚部
+	 * TODO(这里描述这个方法适用条件 – 可选)
+	 * TODO(这里描述这个方法的执行流程 – 可选)
+	 * TODO(这里描述这个方法的使用方法 – 可选)
+	 * TODO(这里描述这个方法的注意事项 – 可选)
+	 *
+	 * @author bme
+	 * @return 虚部
+	 * @since JDK 1.6
+	 */
 	public double getImag() {
 		return imag;
 	}
 
+	/**
+	 * 
+	 * setImag: 设置虚部
+	 * TODO(这里描述这个方法适用条件 – 可选)
+	 * TODO(这里描述这个方法的执行流程 – 可选)
+	 * TODO(这里描述这个方法的使用方法 – 可选)
+	 * TODO(这里描述这个方法的注意事项 – 可选)
+	 *
+	 * @author bme
+	 * @param imag 虚部
+	 * @since JDK 1.6
+	 */
 	public void setImag(double imag) {
 		this.imag = imag;
 	}
 	
+	/**
+	 * 
+	 * set: 设置实部和虚部
+	 * TODO(这里描述这个方法适用条件 – 可选)
+	 * TODO(这里描述这个方法的执行流程 – 可选)
+	 * TODO(这里描述这个方法的使用方法 – 可选)
+	 * TODO(这里描述这个方法的注意事项 – 可选)
+	 *
+	 * @author bme
+	 * @param re 实部
+	 * @param im 虚部
+	 * @since JDK 1.6
+	 */
 	public void set(double re, double im) {
 		real = re;
 		imag = im;
 	}
 	
+	/**
+	 * 
+	 * add: 实部加上一个实数
+	 * TODO(这里描述这个方法适用条件 – 可选)
+	 * TODO(这里描述这个方法的执行流程 – 可选)
+	 * TODO(这里描述这个方法的使用方法 – 可选)
+	 * TODO(这里描述这个方法的注意事项 – 可选)
+	 *
+	 * @author bme
+	 * @param a 实数
+	 * @return this
+	 * @since JDK 1.6
+	 */
 	public Complex add(double a) {
 		real += a;
-		imag += a;
 		return this;
 	}
 	
 	/**
-	 * subtract:减去一个实数
+	 * subtract: 实部减去一个实数
 	 *
 	 * @author bme
-	 * @param a
+	 * @param a 实数
+	 * @return this
 	 * @since JDK 1.6
 	 */
 	public Complex subtract(double a) {
 		real -= a;
-		imag -= a;
 		return this;
 	}
 	
+	/**
+	 * 
+	 * add: 加上一个复数
+	 * TODO(这里描述这个方法适用条件 – 可选)
+	 * TODO(这里描述这个方法的执行流程 – 可选)
+	 * TODO(这里描述这个方法的使用方法 – 可选)
+	 * TODO(这里描述这个方法的注意事项 – 可选)
+	 *
+	 * @author bme
+	 * @param c 复数
+	 * @return this
+	 * @since JDK 1.6
+	 */
 	public Complex add(Complex c) {
 		real += c.real;
 		imag += c.imag;
 		return this;
 	}
 	
+	/**
+	 * 
+	 * subtract: 减去一个复数
+	 * TODO(这里描述这个方法适用条件 – 可选)
+	 * TODO(这里描述这个方法的执行流程 – 可选)
+	 * TODO(这里描述这个方法的使用方法 – 可选)
+	 * TODO(这里描述这个方法的注意事项 – 可选)
+	 *
+	 * @author bme
+	 * @param c 复数
+	 * @return this
+	 * @since JDK 1.6
+	 */
 	public Complex subtract(Complex c) {
 		real -= c.real;
 		imag -= c.real;
 		return this;
 	}
 	
-	
+	/**
+	 * 
+	 * multiple: 实部和虚部都乘以一个实数
+	 * TODO(这里描述这个方法适用条件 – 可选)
+	 * TODO(这里描述这个方法的执行流程 – 可选)
+	 * TODO(这里描述这个方法的使用方法 – 可选)
+	 * TODO(这里描述这个方法的注意事项 – 可选)
+	 *
+	 * @author bme
+	 * @param a 实数
+	 * @return this
+	 * @since JDK 1.6
+	 */
 	public Complex multiple(double a) {
 		real *= a;
 		imag *= a;
 		return this;
 	}
 	
+	/**
+	 * 
+	 * multiple: 乘以一个复数
+	 * TODO(这里描述这个方法适用条件 – 可选)
+	 * TODO(这里描述这个方法的执行流程 – 可选)
+	 * TODO(这里描述这个方法的使用方法 – 可选)
+	 * TODO(这里描述这个方法的注意事项 – 可选)
+	 *
+	 * @author bme
+	 * @param c 复数
+	 * @return this
+	 * @since JDK 1.6
+	 */
 	public Complex multiple(Complex c) {
-		double re = real*c.real - imag*c.imag;
-		double im = real*c.imag + imag*c.real;
-		real = re;
-		imag = im;
+		set(real*c.real - imag*c.imag, real*c.imag + imag*c.real);
 		return this;
 	}
 	
+	/**
+	 * 
+	 * divide: 实部和虚部都除以一个实数
+	 * TODO(这里描述这个方法适用条件 – 可选)
+	 * TODO(这里描述这个方法的执行流程 – 可选)
+	 * TODO(这里描述这个方法的使用方法 – 可选)
+	 * TODO(这里描述这个方法的注意事项 – 可选)
+	 *
+	 * @author bme
+	 * @param a 实数
+	 * @return this
+	 * @since JDK 1.6
+	 */
 	public Complex divide(double a) {
 		real /= a;
 		imag /= a;
 		return this;
 	}
 	
+	/**
+	 * 
+	 * divide: 除以一个复数
+	 * TODO(这里描述这个方法适用条件 – 可选)
+	 * TODO(这里描述这个方法的执行流程 – 可选)
+	 * TODO(这里描述这个方法的使用方法 – 可选)
+	 * TODO(这里描述这个方法的注意事项 – 可选)
+	 *
+	 * @author bme
+	 * @param c 复数
+	 * @return this
+	 * @since JDK 1.6
+	 */
 	public Complex divide(Complex c) {
 		double mod = c.real*c.real + c.imag*c.imag;
-		double re = (real*c.real + imag*c.imag)/mod;
-		double im = (imag*c.real - real*c.imag)/mod;
-		real = re;
-		imag = im;
+		set((real*c.real + imag*c.imag)/mod, (imag*c.real - real*c.imag)/mod);
 		return this;
 	}
 	
+	/**
+	 * 
+	 * abs: 求幅度
+	 * TODO(这里描述这个方法适用条件 – 可选)
+	 * TODO(这里描述这个方法的执行流程 – 可选)
+	 * TODO(这里描述这个方法的使用方法 – 可选)
+	 * TODO(这里描述这个方法的注意事项 – 可选)
+	 *
+	 * @author bme
+	 * @return 幅度
+	 * @since JDK 1.6
+	 */
 	public double abs() {
 		return Math.sqrt(real*real+imag*imag);
 	}
 	
+	/**
+	 * 
+	 * angle: 求相位角弧度，范围[-PI,PI]
+	 * TODO(这里描述这个方法适用条件 – 可选)
+	 * TODO(这里描述这个方法的执行流程 – 可选)
+	 * TODO(这里描述这个方法的使用方法 – 可选)
+	 * TODO(这里描述这个方法的注意事项 – 可选)
+	 *
+	 * @author bme
+	 * @return 相位角弧度
+	 * @since JDK 1.6
+	 */
 	public double angle() {
 		return Math.atan2(imag, real);
 	}
